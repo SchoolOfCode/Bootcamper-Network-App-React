dd61ab2e73b6f11cf5df62e38813a73ee2f299ed
nevillejodie <jlneville92@gmail.com>
2020-04-20 20:21:26 +0100

    tried refactoring it all and it still didnt work :(

--- src/Components/MeetUp/index.js
+++ src/Components/MeetUp/index.js
@@ -1,7 +1,8 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 
-async function Meetup(){
- const [meetup, setMeetup] = useState("") 
+export default function Meetup(){
+
+  const [state, setState] = useState(null)
 
 var myHeaders = new Headers();
 myHeaders.append('Access-Control-Allow-Origin', 'https://api.meetup.com/find/topics?query=tech&only=id,name')
@@ -10,30 +11,27 @@ var myInit = { method: 'GET',
              mode: 'cors',
              cache: 'default' };
 
-var response = await fetch("https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/topics?query=tech&only=id,name", myInit)
-var data = await response.json()
-console.log(data)
+useEffect(() => {
+const loadData = async () => {
+  try {
+const response = await fetch("https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/topics?query=tech&only=id,name", myInit)
+const data = await response.json()
+console.log(`did you get a response?`)
+setState(data)
+   } catch (error) {
+  if (error === "AbortError") {
+    console.log(`error caught`);
+  } else {
+    throw error;
+  }
+}
+};
+loadData();
+}, [])
 
- return (
-    <div>
-      <p>{data[0].name}</p>
-      <br />
-      <cite>MeetUp</cite>
-    </div>
-);
-  
- }
-
-
- /* TO DO:
- Rather making the react component asynchronous, try writing a separate asynchronous function within the component so that the fetch is isolated there.
-
- Then do some conditional rendering with your data so that it doesn't try and render it before the fetch has completed.
- 
- Like {data[0].name  || "dummy data"}
-
- Also, might be worth handling your fetch data in state, but maybe you've not got to that bit yet!
-
- */
-
-  export default Meetup;
+return (
+  <div>
+    <p>{state}</p>
+  </div>
+)
+}
