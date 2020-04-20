2accbc08f9fd5ba7ac858469fdc64799e3422b48
Mell <mellkayrusson@gmail.com>
2020-04-20 15:46:50 +0100

    basic dash finished-ish

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard";
 import "./App.css";
+import Menu from "../Menu/Menu";
 const logo = require("../../images/thisone.png");
 const burgerMenu = require("../../images/burgerMenu.svg");
 
 function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
     <Router>
       <div className="navBar">
         <Link to="/dash">
           <img src={logo} className="logo" alt="school of code logo" />
         </Link>
-        <img src={burgerMenu} className="burgerMenu" alt="menu" />
+        <img
+          src={burgerMenu}
+          className="burgerMenu"
+          alt="menu"
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+        />
+        {isMenuOpen && <Menu />}
         <hr />
       </div>
       <Switch>
         <Route path="/dash">
           <Dashboard />
+        </Route>
+        <Route path="/menu">
+          <Menu />
         </Route>
       </Switch>
     </Router>
