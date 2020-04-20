45bb4dada84466f91c1946052203b22ff144e470
nevillejodie <jlneville92@gmail.com>
2020-04-20 19:38:02 +0100

    tips from tao

--- src/Components/MeetUp/index.js
+++ src/Components/MeetUp/index.js
@@ -12,7 +12,7 @@ var myInit = { method: 'GET',
 
 var response = await fetch("https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/topics?query=tech&only=id,name", myInit)
 var data = await response.json()
-console.log(data[0].name)
+console.log(data)
 
  return (
     <div>
@@ -23,5 +23,17 @@ console.log(data[0].name)
 );
   
  }
+
+
+ /* TO DO:
+ Rather making the react component asynchronous, try writing a separate asynchronous function within the component so that the fetch is isolated there.
+
+ Then do some conditional rendering with your data so that it doesn't try and render it before the fetch has completed.
+ 
+ Like {data[0].name  || "dummy data"}
+
+ Also, might be worth handling your fetch data in state, but maybe you've not got to that bit yet!
+
+ */
 
   export default Meetup;
