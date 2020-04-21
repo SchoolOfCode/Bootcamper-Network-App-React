fabd17bd5ae80024d40ee5f53c3b429cf90f4412
nevillejodie <jlneville92@gmail.com>
2020-04-21 15:49:40 +0100

    committing

--- src/Components/MeetUp/index.js
+++ src/Components/MeetUp/index.js
@@ -1,13 +1,11 @@
 import React, {useState, useEffect} from "react";
+import Events from '../Events'
+
 
 export default function Meetup(){
 
   const [state, setState] = useState()
-/*   const [state1, setState1] = useState()
-  const [state2, setState2] = useState()
-  const [state3, setState3] = useState()
-  const [state4, setState4] = useState()
-  const [state5, setState5] = useState() */
+
 
 var myHeaders = new Headers();
 myHeaders.append('Access-Control-Allow-Origin', 'https://api.meetup.com/find/topics?query=tech&only=id,name')
@@ -19,7 +17,7 @@ var myInit = { method: 'GET',
 useEffect(() => {
 const loadData = async () => {
   try {
-const response = await fetch("https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/events/?allMeetups=false&keywords=tech&radius=5&userFreeform=Birmingham%2C+West+Midlands%2C+England%2C+United+Kingdom&mcId=c1012719&change=yes&eventFilter=mysugg", myInit)
+const response = await fetch("https://cors-anywhere.herokuapp.com/https://api.meetup.com/find/topics?zip=&radius=1&query=tech&only=id,name", myInit)
 
 /* https://www.meetup.com/find/events/?allMeetups=false&keywords=tech&radius=5&userFreeform=Birmingham%2C+West+Midlands%2C+England%2C+United+Kingdom&mcId=c1012719&change=yes&eventFilter=mysugg */
 
@@ -32,12 +30,8 @@ const response = await fetch("https://cors-anywhere.herokuapp.com/https://api.me
 
 const data = await response.json()
 console.log(data)
-setState(data[0].name)
-/* setState1(data[1].name)
-setState2(data[2].name)
-setState3(data[3].name)
-setState4(data[4].name)
-setState5(data[5].name) */
+setState(data) 
+
    } catch (error) {
   if (error === "AbortError") {
     console.log(`error caught`);
@@ -51,9 +45,7 @@ loadData();
 
 return (
   <div>
-  <ul>
-    <li>{state}</li>
-    </ul>
+<Events/>
   </div>
 )
 }
