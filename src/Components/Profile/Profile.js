227b6c5784f727ee026678b46b051f855bca5e72
Mell <mellkayrusson@gmail.com>
2020-04-21 11:01:53 +0100

    icons linked to relevant profiles

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -1,12 +1,19 @@
 import React from "react";
 import "./Profile.css";
 
+const twitter = require("../../images/twitter-logo.png");
+const linkedin = require("../../images/linkedin.png");
+const github = require("../../images/github.png");
+
 const dummyData = {
   name: "Jodie Neville",
   region: "Birmingham",
   cohort: 3,
   about:
     "About Me I was on the School of Codes 3rd Cohort and am based in Birmingham.",
+  twitter: "https://twitter.com/JodieNeville",
+  linkedin: "https://www.linkedin.com/in/jlneville/",
+  github: "https://github.com/nevillejodie",
 };
 
 function Profile() {
@@ -14,6 +21,24 @@ function Profile() {
     <>
       <div className="info">
         <h2> {dummyData.name} </h2>
+        <img
+          src={twitter}
+          alt="twitter logo"
+          className="icons"
+          onClick={() => window.location.assign(dummyData.twitter)}
+        />
+        <img
+          src={linkedin}
+          alt="linkedin logo"
+          className="icons"
+          onClick={() => window.location.assign(dummyData.linkedin)}
+        />
+        <img
+          src={github}
+          alt="github logo"
+          className="icons"
+          onClick={() => window.location.assign(dummyData.github)}
+        />
       </div>
       <div className="profileContainer"></div>
     </>
