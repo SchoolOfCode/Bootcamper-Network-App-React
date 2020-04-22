37c650debd31dc980e2c79093b0450cbc491a155
Mell <mellkayrusson@gmail.com>
2020-04-22 12:12:11 +0100

    quick fixes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Dashboard from "../Dashboard/Dashboard";
 import "./App.css";
-import Menu from "../Menu/Menu";
 import NavBar from "../NavBar/NavBar";
 import Profile from "../Profile/Profile";
 import CompaniesPage from "../CompaniesPage/CompaniesPage";
