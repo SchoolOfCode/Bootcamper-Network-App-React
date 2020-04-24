9049b8db97ddf55da9caff3a56b78f94b2dafee5
nevillejodie <jlneville92@gmail.com>
2020-04-24 15:06:12 +0100

    delete your node modules and re install

--- src/Components/CreateProfile/createprofile.test.js
+++ src/Components/CreateProfile/createprofile.test.js
@@ -1,3 +1,8 @@
+import React from "react";
+import { render } from "@testing-library/react";
+
+import ProfileInputs from "./index";
+
 /* 
 
 TO DO 
@@ -23,13 +28,39 @@ the form will:
 - the user selects the name, but the id is used to render it on their profile
 - include a button to add a company tbc. 
 
-
 - TEST e.target.value from the input is the same value as is being rendered on screen 
 - TEST what their typing is saved into initial this.state.
 - TEST POST and PATCH ???
 - TEST fetch beings back all company names 
 
+snapshot - save what the component looks like at first and will compare other tests to this first verison
 
+state ?? you can test effects of stuff e.g. if i click a button the text changes, if i click 5 times 5 is shown here etc. 
 
+usereducer is good for testing 
 
+function reducer(state, action) {
+//...
+switch(action.type) {
+  case "INPUT_CHANGED":
+    return {...state, [action.payload.name]: action.payload.value};
+}
+return state;
+}
+
+const actual = reducer({input: "hello"}, {type: "INPUT_CHANGED", payload: {name: "email", value: "chris@"}})
+
+actual.input === "goodbye"
 */
+
+it("should check if the first name label is there", () => {
+  const { getByText } = render(<ProfileInputs />);
+  const labelElement = getByText(/First Name:/i);
+  expect(labelElement).toBeInTheDocument();
+});
+
+/* it("check input value is same as state", () => {
+const expected = initialState.first_name;
+const actual = input.first_name;
+expect(actual).toStrictEqual(expected);
+}) */
--- src/Components/CreateProfile/index.js
+++ src/Components/CreateProfile/index.js
@@ -1,12 +1,44 @@
 import React, {useState} from 'react'
 
 const initialState = {
-    firstname: ""
-    surname: ""
+    first_name: "",
+    surname: "",
+    profile: "",
+    job_title: "",
+    company_id: 0,
+    salary: "",
+    start_date: "",
+    previous_roles: "",
+    cohort_num: 0,
+    region: "",
+    job_satisfaction: "",
+    new_job: "",
+    twitter: "",
+    github: "",
+    portfolio: "",
+    linkedin: ""
 }
 
 function ProfileInputs() {
-    
+    const [formState, setFormState] = useState(initialState)
+
+    function handleSubmit(){
+    }
+
+    function handleChange(){
+        
+    }
+
+    return <div>
+        <form
+        onSubmit={handleSubmit}
+        style={{ display: "flex", flexDirection: "column" }}>
+      <label>First Name:<input type="text" placeholder="First Name" name="first_name" value={formState.first_name} onChange={handleChange}/></label>
+
+
+      </form>
+    </div>
 }
+
 
 export default ProfileInputs;
