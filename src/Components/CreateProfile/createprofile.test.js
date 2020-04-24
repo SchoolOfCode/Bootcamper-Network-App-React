31d6799df5e6407118e64277a4e82074ab02a3c2
nevillejodie <jlneville92@gmail.com>
2020-04-24 11:46:09 +0100

    to do list

---
+++ src/Components/CreateProfile/createprofile.test.js
@@ -0,0 +1,34 @@
+/* 
+
+TO DO 
+the form will: 
+- click a button
+- lots of inputs for different things
+- if they click edit, it shows up all of their existing info
+- if they are creating a profile, these inputs are empty 
+- new members should be directed straight to the sign up form 
+- initial state of all of them is held in one 
+- conditional render = if this user name doesn't already exist, direct them to sign up
+- if the user already exists, take them to their profile 
+- initial state should be in an object 
+- see one ben made earlier 
+- make a form with a bunch of inputs in, to include uploading a profile pic 
+- when it's filled in/changes are made onClick does the fetch (POST and PATCH)
+- first time registers = POST 
+- editing will be a = PATCH 
+- onClick the POST or PATCH is complete and it will show the user their profile
+- company = dropdown list to come from a fetch request of all conmpanies.
+- in the company fetch bring back company name and company id 
+- user will see the company name, but the value will be the id
+- the user selects the name, but the id is used to render it on their profile
+- include a button to add a company tbc. 
+
+
+- TEST e.target.value from the input is the same value as is being rendered on screen 
+- TEST what their typing is saved into initial this.state.
+- TEST POST and PATCH ???
+- TEST fetch beings back all company names 
+
+
+
+*/
