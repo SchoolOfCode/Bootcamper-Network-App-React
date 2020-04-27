f7a6164e502f4d1edc401b2bd98b31d92c5e6fed
Helen Kent <helen8297@gmail.com>
2020-04-27 14:57:25 +0100

    firebase page added

---
+++ src/Components/firebase.js
@@ -0,0 +1,44 @@
+import firebase from "firebase/app";
+import "firebase/auth";
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+});
+
+firebase.analytics();
+
+var provider = new firebase.auth.GoogleAuthProvider();
+
+export function signInWithPopup() {
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(function (result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+    })
+    .catch(function (error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+    });
+}
+
+export function onAuthStateChanged(callback) {
+  firebase.auth().onAuthStateChanged(callback);
+}
+
+export function logout() {
+  return firebase.auth().signOut().catch(console.error);
+}
