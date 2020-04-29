8b924201f04248583b7f20c22fc6eaf4c8bf1bc0
Mell <mellkayrusson@gmail.com>
2020-04-29 15:55:19 +0100

    meetup changes

--- src/Components/MeetUp/index.js
+++ src/Components/MeetUp/index.js
@@ -8,13 +8,23 @@ export default function Meetup({ state }) {
   return (
     <>
       <div className={css.header}>
-        <h2> Events </h2>
+        <h2 className={css.h2}> Events </h2>
+        <small>
+          Clicking the 'I'm attending' button will not sign you up for the event
+          on Meetup, it will only notify people on this app that you're
+          attending. Click the titles to go to their Meetup Pages.
+        </small>
       </div>
       <div className={morecss.bigContainer}>
         {state.map((item) => {
           return (
             <>
-              <EventsPage name={item.name} />
+              <EventsPage
+                name={item.name}
+                link={item.link}
+                date={item.local_date}
+                time={item.local_time}
+              />
             </>
           );
         })}
