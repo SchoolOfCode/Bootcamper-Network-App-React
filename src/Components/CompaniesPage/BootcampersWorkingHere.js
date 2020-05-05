bc710021310deb691a9d7ba0bd71140889000b4f
nevillejodie <jlneville92@gmail.com>
2020-05-05 19:36:10 +0100

    link to profile in bootcampers who work here

--- src/Components/CompaniesPage/BootcampersWorkingHere.js
+++ src/Components/CompaniesPage/BootcampersWorkingHere.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { URL } from "../../config";
 import { useParams } from "react-router-dom";
 import css from "./bcmpr.module.css"
+import { Link } from "react-router-dom";
 
 
 function BootcampersWorkingHere({ companyname }) {
@@ -34,7 +35,9 @@ function BootcampersWorkingHere({ companyname }) {
         {bootcamperWorkingHere.map((item) => {
           return (
             <li>
-              {item.first_name} {item.surname} <img className={css.bootcamperPic} alt="" src={item.photourl} />
+            <Link to={`/profiles/${item.bootcamper_id}`} className={css.link}>
+              {item.first_name} {item.surname} 
+              </Link> <img className={css.bootcamperPic} alt="" src={item.photourl} />
             </li>
           )
         })}
