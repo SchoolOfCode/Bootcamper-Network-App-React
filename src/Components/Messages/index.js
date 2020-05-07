a306229773138d639afe73ba19d7391b610c6457
Mell <mellkayrusson@gmail.com>
2020-05-07 12:31:51 +0100

    fixed css from sockets

--- src/Components/Messages/index.js
+++ src/Components/Messages/index.js
@@ -1,41 +1,44 @@
-import React from 'react'
-import css from './message.module.css'
-import io from "socket.io-client"
+import React from "react";
+import css from "./message.module.css";
+import io from "socket.io-client";
 
-const connection = io("http://localhost:5000")
-
-
+const connection = io("http://localhost:5000");
 
 const Messages = () => {
+  var socket = io();
+  const button = document.querySelector("button");
+  const ul = document.querySelector("ul");
+  const message = document.querySelector("#m");
 
+  function handleClick(e) {
+    e.preventDefault();
+    socket.emit("chat message", message.value);
+    console.log(message.value);
+  }
 
-    var socket = io()
-    const button = document.querySelector("button");
-    const ul = document.querySelector("ul");
-    const message = document.querySelector("#m")
-    
-function handleClick(e) {
-e.preventDefault();
-socket.emit("chat message", message.value);
-console.log(message.value)
-        }
+  socket.on("chat message", displayMsg);
+  function displayMsg(msg) {
+    const li = document.createElement("li");
+    li.innerText = msg;
+    ul.appendChild(li);
+  }
 
-    socket.on("chat message", displayMsg);
-    function displayMsg(msg) {
-        const li = document.createElement("li");
-        li.innerText = msg;
-        ul.appendChild(li)
-    }
+  return (
+    <div className={css.div}>
+      <ul className={css.messages}></ul>
+      <form action="" className={css.form}>
+        <input className={css.forminput} autocomplete="off" />
+        <button
+          onSubmit={() => {
+            handleClick();
+          }}
+          className={css.formbutton}
+        >
+          Send
+        </button>
+      </form>
+    </div>
+  );
+};
 
-
-    return (
-        <div className={css.div}>
-           <ul id={css.messages}></ul>
-    <form action="">
-      <input id={css.m} autocomplete="off" /><button onSubmit={() => {handleClick()}}>Send</button>
-    </form>
-        </div>
-    )
-    }
-
-export default Messages
+export default Messages;
