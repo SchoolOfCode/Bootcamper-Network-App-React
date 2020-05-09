c0df6871349f819e720af3026658ea830184d5a5
Helen Kent <helen8297@gmail.com>
2020-05-09 15:07:11 +0100

    added to messages

--- src/Components/Messages/index.js
+++ src/Components/Messages/index.js
@@ -1,41 +1,50 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useContext } from "react";
 import css from "./message.module.css";
+import { ProfileContext } from "../../config";
 import io from "socket.io-client";
 
-const connection = io(
-  "http://bootcampcommunityapp-dev.eu-west-1.elasticbeanstalk.com"
-);
+const connection = io("http://localhost:5000");
+
+//"http://bootcampcommunityapp-dev.eu-west-1.elasticbeanstalk.com"
 
 const Messages = () => {
   const [input, setInput] = useState("");
   const [allMessages, setAllMessages] = useState([]);
+  const { userProfile } = useContext(ProfileContext);
+
+  console.log(`messages userdata?`, userProfile);
 
   function handleInput(event) {
     setInput(event.target.value);
   }
 
   useEffect(() => {
-    connection.on("chatMessage", ({ message }) => {
-      displayMsg(message.input);
-      console.log(`message after sending`, message);
+    connection.on("chatMessage", (myMessage) => {
+      // displayMsg(message.input);
+      console.log(`Receiving message from backend`, myMessage);
     });
   }, []);
 
   //when receiving a message
   function displayMsg(msg) {
-    console.log(`this is MESSAGE`, msg);
+    console.log(`Message for display function`, msg);
     setAllMessages([...allMessages, msg]);
   }
-  console.log(`message array`, allMessages);
+  // console.log(`message array`, allMessages);
 
   //sending message
   function sendMessage() {
-    connection.emit("chatMessage", { input });
+    const myMessage = { Message: input, Name: userProfile.first_name };
+    connection.emit("chatMessage", myMessage);
+    console.log("sending message: ", myMessage);
     setInput("");
   }
 
-  //store messages in server
-  //on connection, server sends messages that you've had
+  //check format of received input on the backend
+  //add firstname photourl & boocamper_id & time/date sent to messages being sent
+  //check to see if sent message is also received back to user
+  //if they are, add them to an array
+  //map over the array to display the messages
 
   return (
     <div className={css.div}>
@@ -45,9 +54,9 @@ const Messages = () => {
           onChange={handleInput}
           value={input}
           className={css.forminput}
-          autocomplete="off"
+          // autocomplete="off"
         />
-        <button className={css.formbutton} onClick={sendMessage}>
+        <button className={css.formbutton} type="button" onClick={sendMessage}>
           Send
         </button>
       </form>
