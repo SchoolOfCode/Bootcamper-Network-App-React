499878cd685bf6b4f333791e3bf860cef55628da
Mell <mellkayrusson@gmail.com>
2020-05-10 13:35:03 +0100

    admin dash mostly done

--- src/Components/AdminDash/index.js
+++ src/Components/AdminDash/index.js
@@ -5,9 +5,10 @@ import { Divider } from "@material-ui/core";
 
 function AdminDash() {
   const [jobSatisfactionData, setjobSatisfactionData] = useState([]);
+  const [newJobData, setNewJobData] = useState([]);
 
   useEffect(() => {
-    async function getJobSatisfactoionData() {
+    async function getJobSatisfactionData() {
       try {
         const res = await fetch(`${URL}/bootcampers/jobsatisfaction`);
         const data = await res.json();
@@ -18,48 +19,81 @@ function AdminDash() {
         console.log(`fetch error`, err);
       }
     }
-    getJobSatisfactoionData();
+    getJobSatisfactionData();
+  }, []);
+  useEffect(() => {
+    async function getNewJobData() {
+      try {
+        const res = await fetch(`${URL}/bootcampers/newjob`);
+        const data = await res.json();
+        if (data.payload) {
+          setNewJobData(data.payload);
+        }
+      } catch (err) {
+        console.log(`fetch error`, err);
+      }
+    }
+    getNewJobData();
   }, []);
 
-  //   const {
-  //     first_name,
-  //     surname,
-  //     cohort_num,
-  //     company_name,
-  //     salary,
-  //     photo_url,
-  //     job_title,
-  //     job_satisfaction,
-  //     new_job,
-  //   } = jobSatisfactionData;
-
   return (
-    <div className={css.otherContainer}>
-      <h3> Bootcampers with a Job Satisfaction rating less than 3:</h3>
-      {jobSatisfactionData.map((item) => {
-        return (
-          <>
-            <img src={item.photo_url} className={css.profilePic} alt="" />
-            <p className={css.listItem}>
-              {item.first_name} {item.surname}
-            </p>
-            <li className={css.listItem}>
-              <span>Cohort: </span>
-              {item.cohort_num}
-            </li>
-            <li className={css.listItem}>
-              <span>Company Name:</span>
-              {item.company_name}
-            </li>
-            <li className={css.listItem}>
-              <span>Job Satisfaction: </span>
-              {item.job_satisfaction}
-            </li>
-            <Divider />
-          </>
-        );
-      })}
-    </div>
+    <>
+      <div className={css.otherContainer}>
+        <h4 className={css.adminHeader}>
+          {" "}
+          Bootcampers with a Job Satisfaction rating less than 3:
+        </h4>
+        {jobSatisfactionData.map((item) => {
+          return (
+            <>
+              <img src={item.photo_url} className={css.profilePic} alt="" />
+              <p className={css.name}>
+                {item.first_name} {item.surname}
+              </p>
+              <li className={css.listItem}>
+                <span>Cohort: </span>
+                {item.cohort_num}
+              </li>
+              <li className={css.listItem}>
+                <span>Company Name:</span>
+                {item.company_name}
+              </li>
+              <li className={css.listItem}>
+                <span>Job Satisfaction: </span>
+                {item.job_satisfaction}
+              </li>
+              <Divider />
+            </>
+          );
+        })}
+      </div>
+      <div className={css.otherContainer}>
+        <h4 className={css.adminHeader}> Bootcampers Who Want a new Job: </h4>
+        {newJobData.map((item) => {
+          return (
+            <>
+              <img src={item.photo_url} className={css.profilePic} alt="" />
+              <p className={css.name}>
+                {item.first_name} {item.surname}
+              </p>
+              <li className={css.listItem}>
+                <span>Cohort: </span>
+                {item.cohort_num}
+              </li>
+              <li className={css.listItem}>
+                <span>Company Name:</span>
+                {item.company_name}
+              </li>
+              <li className={css.listItem}>
+                <span>Job Satisfaction: </span>
+                {item.job_satisfaction}
+              </li>
+              <Divider />
+            </>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
