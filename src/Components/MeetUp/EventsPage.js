03594d281ccd3d4d7b5e25ee77bb40472a540938
Mell <mellkayrusson@gmail.com>
2020-05-12 09:41:32 +0100

    formatted the date and added a location to the events that have one

--- src/Components/MeetUp/EventsPage.js
+++ src/Components/MeetUp/EventsPage.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import cn from "classnames";
 import css from "./EventsPage.module.css";
 
-function EventsPage({ name, link, date, time }) {
+function EventsPage({ name, link, date, time, venue }) {
   const [isClicked, setIsClicked] = useState(false);
   function handleClick() {
     setIsClicked(!isClicked);
@@ -13,8 +13,9 @@ function EventsPage({ name, link, date, time }) {
         <a href={link} className={css.link}>
           <p className={css.eventName}>{name}</p>
         </a>
-        <p className={css.subheader}>{date}</p>
+        <p className={css.subheader}>{date.split("-").reverse().join("-")}</p>
         <p className={css.subheader}>{time}</p>
+        <p className={css.subheader}>{venue}</p>
 
         {/* <button
           className={cn({ [css.clicked]: isClicked })}
--- src/Components/MeetUp/index.js
+++ src/Components/MeetUp/index.js
@@ -24,6 +24,7 @@ export default function Meetup({ state }) {
               link={item.link}
               date={item.local_date}
               time={item.local_time}
+              venue={item?.venue?.name}
             />
           );
         })}
