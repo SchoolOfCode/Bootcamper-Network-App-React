95dec0d56f39c49f25c7effbf36fc0551012a222
nevillejodie <jlneville92@gmail.com>
2020-05-12 09:52:25 +0100

    useful links

--- src/Components/UsefulLinks/index.js
+++ src/Components/UsefulLinks/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import css from "./usefulLinks.module.css"
-import Divider from "../Divider";
+/* import Divider from "../Divider"; */
 import webPic from "./web.jpg"
-import stackPic from "./stack.png"
+import stackPic from "./stack.jpg"
 import slackPic from "./slack.png"
 import linkedInPic from "./linkedin.png"
 
@@ -11,32 +11,31 @@ function UsefulLinks() {
     <>
       <div className={css.header}>
         <h2 className={css.h2}> Useful Links</h2>
-        <small className={css.subheader}>
-          Some links we think might be useful to any bootcampers, past or
+        <p className={css.subheader}>
+          Links to School of Code sites we think might be useful to any bootcampers, past or
           present.
-        </small>
+        </p>
       </div>
       <div className={css.bigContainer}>
         <p className={css.extraData}>
-<img height="25px"  alt="" src={webPic}/>
-          <span> <a className={css.linkText} href="https://www.schoolofcode.com/"> School of Code Website </a> </span>
+
+           <a className={css.linkText} href="https://www.schoolofcode.com/"> <img height="45px" alt="" src={webPic}/> </a> 
           
         </p>
         {/* <Divider /> */}
         <p className={css.extraData}>
-        <img height="25px" alt="" src={stackPic}/>
-          <span> <a className={css.linkText} href="https://stackoverflow.com/c/school-of-code/questions"> School of Code Stack Overflow </a></span>
+        
+          <span> <a className={css.linkText} href="https://stackoverflow.com/c/school-of-code/questions"> <img height="35px" alt="" src="https://cdn.sstatic.net/Sites/stackoverflow/company/Img/logos/so/so-logo.png?v=9c558ec15d8a"/> </a></span>
         </p>
         {/* <Divider /> */}
         <p className={css.extraData}>
-        <img height="25px" alt="" src={slackPic}/>
-          <span> <a className={css.linkText} href="https://schoolofcodebootcamp.slack.com"> School of Code Slack </a> </span>
+        
+          <span> <a className={css.linkText} href="https://schoolofcodebootcamp.slack.com"> <img height="45px" alt="" src={slackPic}/> </a> </span>
           
         </p>
         {/* <Divider /> */}
         <p className={css.extraData}>
-        <img height="25px" alt="" src={linkedInPic}/>
-          <span> <a className={css.linkText} href="https://schoolofcodebootcamp.slack.com"> School of Code LinkedIn </a> </span>
+          <span> <a className={css.linkText} href="https://schoolofcodebootcamp.slack.com"> <img height="45px" alt="" src={linkedInPic}/> </a> </span>
           
         </p>
       </div>
