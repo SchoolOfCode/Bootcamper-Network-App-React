654a5248dff8fc6eb89a284b16b25190b2856b6a
Mell <mellkayrusson@gmail.com>
2020-05-13 09:43:31 +0100

    links in a grid

--- src/Components/UsefulLinks/index.js
+++ src/Components/UsefulLinks/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import css from "./usefulLinks.module.css"
+import css from "./usefulLinks.module.css";
 /* import Divider from "../Divider"; */
-import webPic from "./web.jpg"
-import stackPic from "./stack.jpg"
-import slackPic from "./slack.png"
-import linkedInPic from "./linkedin.png"
+import webPic from "./web.jpg";
+import stackPic from "./stack.jpg";
+import slackPic from "./slack.png";
+import linkedInPic from "./linkedin.png";
 
 function UsefulLinks() {
   return (
@@ -12,31 +12,45 @@ function UsefulLinks() {
       <div className={css.header}>
         <h2 className={css.h2}> Useful Links</h2>
         <p className={css.subheader}>
-          Links to School of Code sites we think might be useful to any bootcampers, past or
-          present.
+          Links to School of Code sites we think might be useful to any
+          bootcampers, past or present.
         </p>
       </div>
-      <div className={css.bigContainer}>
-        <p className={css.extraData}>
+      <div className={css.grid}>
+        <p className={css.box}>
+          <a className={css.linkText} href="https://www.schoolofcode.com/">
+            <img height="45px" alt="" src={webPic} />
+          </a>
+        </p>
+        <p className={css.box}>
+          <a
+            className={css.linkText}
+            href="https://stackoverflow.com/c/school-of-code/questions"
+          >
+            <img
+              height="35px"
+              alt=""
+              src="https://cdn.sstatic.net/Sites/stackoverflow/company/Img/logos/so/so-logo.png?v=9c558ec15d8a"
+            />
+          </a>
+        </p>
 
-           <a className={css.linkText} href="https://www.schoolofcode.com/"> <img height="45px" alt="" src={webPic}/> </a> 
-          
+        <p className={css.box}>
+          <a
+            className={css.linkText}
+            href="https://schoolofcodebootcamp.slack.com"
+          >
+            <img height="45px" alt="" src={slackPic} />
+          </a>
         </p>
-        {/* <Divider /> */}
-        <p className={css.extraData}>
-        
-          <span> <a className={css.linkText} href="https://stackoverflow.com/c/school-of-code/questions"> <img height="35px" alt="" src="https://cdn.sstatic.net/Sites/stackoverflow/company/Img/logos/so/so-logo.png?v=9c558ec15d8a"/> </a></span>
-        </p>
-        {/* <Divider /> */}
-        <p className={css.extraData}>
-        
-          <span> <a className={css.linkText} href="https://schoolofcodebootcamp.slack.com"> <img height="45px" alt="" src={slackPic}/> </a> </span>
-          
-        </p>
-        {/* <Divider /> */}
-        <p className={css.extraData}>
-          <span> <a className={css.linkText} href="https://schoolofcodebootcamp.slack.com"> <img height="45px" alt="" src={linkedInPic}/> </a> </span>
-          
+
+        <p className={css.box}>
+          <a
+            className={css.linkText}
+            href="https://schoolofcodebootcamp.slack.com"
+          >
+            <img height="45px" alt="" src={linkedInPic} />
+          </a>
         </p>
       </div>
     </>
